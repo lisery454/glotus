ebe796b82b89aab1f752fde053b95797ee78f740
root <root@lisery-zhaoyang>
2025-07-18 16:42:03 +0800

    fix: 修改lib的公开方法

--- glotus/src/lib.rs
+++ glotus/src/lib.rs
@@ -1,11 +1,21 @@
 mod app;
 mod app_builder;
-pub mod camera;
-pub mod entity;
+mod camera;
+mod entity;
 mod log_builder;
-pub mod material;
-pub mod mesh;
-pub mod shader;
-pub mod transform;
+mod material;
+mod mesh;
+mod shader;
+mod transform;
 
 pub use app_builder::AppBuilder;
+
+use crate::{mesh::vertex::Vertex, transform::Transform};
+
+pub fn get_transform_zero() -> Transform {
+    Transform::new()
+}
+
+pub fn get_vertex_from_position(x: f32, y: f32, z: f32) -> Vertex {
+    Vertex::from_position(x, y, z)
+}
